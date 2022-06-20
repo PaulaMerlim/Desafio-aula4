6c9bab0099b4b266422298d62c2ef67795eb7616
PaulaMerlim <paula.merlim.77@gmail.com>
2022-06-20 19:11:43 -0300

    subindo meu desafio aula 4

---
+++ script.js
@@ -0,0 +1,40 @@
+ //1 - crie uma função que exiba uma mensagem no console
+ function MeuPerfil (){
+    console.log ("Meu Perfil");
+ }
+ MeuPerfil ()
+ console.log ("..................................................")
+ // crie uma função que receba o seu nome como (parâmetro) e exiba no console.
+
+ function ChamarNome (Nome){
+    console.log ('Olá '+ Nome +',seja bem vinda ao VnW')
+ }
+ ChamarNome ("Ana Paula")
+ console.log ("..................................................")
+
+ //crie uma função que receba nome, idade, e um estilo musical preferido (parâmetros) e exiba no console.
+
+ function Perfil (Nome, Idade, EstiloMusical){
+console.log ('o nome é '+Nome+', a idade é,  '+Idade+', e gosta do estilo musical,'+EstiloMusical)
+ }
+ Perfil ('Ana Paula','45','MPB')
+
+ console.log ("..................................................")
+
+ // crie uma função que receba um filme, uma música (parâmetros) e exiba no console.
+ 
+ function Entretenimento (filme, musica){
+    console.log ('o nome do filme é, '+filme+' e o nome da musica é, '+musica)
+ }
+Entretenimento ('The Lion King', 'Hakuna Matata')
+
+console.log ("..................................................")
+
+// crie uma função que retorne o triplo do número recebido no parâmetro da função
+
+ function x3 (x){
+    console.log ('O triplo de', + x + ' é ' , x *3)
+ }
+ x3 (2)
+
+
